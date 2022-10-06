52b21c7a8104f5b02a6a202ce7b6fe3b902c7ff8
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-06 21:46:56 +0530

    Add files via upload
    added uc1 and program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputeProblem
+{
+    internal class Program
+    {
+        public static void Main()
+        { 
+           Console.WriteLine("Welcome to Line Comparison Computation Program");
+            //UC1_Line.Main1();
+            //Uc2_line.Main1();
+            Uc3_line.Main2();
+
+
+        }
+
+    }
+}
---
+++ UC1_Line.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComputeProblem
+{
+    class UC1_Line
+    {
+        public static void Main1()
+        {   //line end points
+            Double x1,y1,x2,y2,L1 ;
+            Console.WriteLine("enter end points of line 01 starting point (x1,y1)and ending point (x2,y2) values");  
+            x1=Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+           //Length of a Line (L1)= sqrt((x2 - x1) ^ 2 + (y2- y1) ^ 2)
+            L1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("linelength is " + L1);
+          }
+        
+    }
+}
